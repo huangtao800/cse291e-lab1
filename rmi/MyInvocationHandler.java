ae46a761fb881ef0603fe07c33a8d60bc991f3a6
huangtao800 <huangtao800@gmail.com>
2016-04-15 15:36:47 -0700

    Add InvocationHandler

---
+++ rmi/MyInvocationHandler.java
@@ -0,0 +1,14 @@
+package rmi;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+
+/**
+ * Created by tao on 4/15/16.
+ */
+public class MyInvocationHandler implements InvocationHandler {
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        return null;
+    }
+}
