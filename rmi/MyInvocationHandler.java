c3a76100a78f9b784e71e2d963409f0f6b40de4b
huangtao800 <huangtao800@gmail.com>
2016-04-21 23:42:41 -0700

    Implement writeObject

--- rmi/MyInvocationHandler.java
+++ rmi/MyInvocationHandler.java
@@ -1,6 +1,6 @@
 package rmi;
 
-import java.io.IOException;
+import java.io.*;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.net.InetSocketAddress;
@@ -24,6 +24,19 @@ public class MyInvocationHandler implements InvocationHandler {
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
         // I think this proxy object is useless, since it should call RMI    -- Tao
         String methodName = method.getName();
+        Serializable[] argList = new Serializable[args.length+1];
+        argList[0] = methodName;
+        try{
+            for(int i=0;i<args.length;i++){
+                Serializable t = (Serializable) args[i];
+                argList[i] = t;
+            }
+        }catch (ClassCastException e){
+            throw new NotSerializableException("Argument(s) Not Serializable");
+        }
+
+        ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
+        oos.writeObject(argList);
 
         return null;
     }
